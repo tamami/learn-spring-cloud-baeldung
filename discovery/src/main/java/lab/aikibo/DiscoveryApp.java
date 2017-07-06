3279ca2bbe18825b0622a82086f155d75835c449
tamami <tamami.oka@gmail.com>
2017-07-06 23:43:45 +0700

    update content

---
+++ discovery/src/main/java/lab/aikibo/DiscoveryApp.java
@@ -0,0 +1,19 @@
+package lab.aikibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DiscoveryApp.class, args);
+    }
+}
